9d463a2b3f3d115ce697b252029d4ea67bc268d1
NFarrell87 <52046779+nfarrell87@users.noreply.github.com>
2019-09-17 10:06:13 -0700

    updated from laptop

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,39 @@
 var friends = require("../data/friends.js");
 
+let newFriends = friends
 module.exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends);
-      });
+    });
+
+
+
     app.post("/api/friends", function (req, res) {
-        // req.body hosts is equal to the JSON post sent from the user
-        // This works because of our body parsing middleware
-        friends.push(req.body);
-        res.json(friends);
+        newFriends.push(req.body);
+        
         console.log(friends);
+        var user = req.body;
+
+        for (var i = 0; i < user.scores.length; i++) {
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+
+
+        var bestMatch = 0;
+        var minimumDifference = 40;
+
+        for (var i = 0; i < friends.length; i++) {
+            var totalDifference = 0;
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+                totalDifference += difference;
+            }
+            if (totalDifference < minimumDifference) {
+                bestMatch = i;
+                minimumDifference = totalDifference;
+            }
+        }
+        res.json(newFriends);
     });
 }
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var app = express();
+
 var PORT = process.env.PORT || 8080;
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 
 app.listen(PORT, function() {
 	console.log("App listening on port " + PORT);
